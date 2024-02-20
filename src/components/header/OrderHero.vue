8f07e7c5236010e8453236e741b1bcdf4411a7b0
Roma Tolkachev <tolkachevroman@bk.ru>
2024-02-20 22:44:23 +0300

    fixing sizes

--- src/components/header/OrderHero.vue
+++ src/components/header/OrderHero.vue
@@ -20,24 +20,24 @@ onClickOutside(orderRef, () => {
 
 <template>
   <section class="h-[calc(100svh-65px)] w-full fixed left-1/2 -translate-x-1/2 container py-6">
-    <div ref="orderRef" class="bg-gray-burger mx-auto w-full max-w-3xl h-full rounded-xl p-2.5">
-      <div class="flex justify-end">
-        <button v-on:click="toggleOrderWindow" class="rounded-xl active:bg-gray-light md:active:bg-gray-burger w-fit p-2.5">
-          <span class="w-fit select-none text-gray-dark">закрыть</span>
-        </button>
-      </div>
-      <form class="flex [&>*>textarea]:resize-none flex-col w-full [&>*>textarea]:text-gray-dark [&>*>textarea]:border-2 [&>*>textarea]:border-white [&>*>textarea:focus]:border-gray-dark [&>*>textarea:focus]:outline-none [&>*>textarea]:focus:border-pink-500 [&>*>textarea]:w-full [&>*>textarea]:rounded-xl [&>*>textarea]:p-2.5 gap-5" action="">
-        <label class="mt-2.5" for="">
-          <textarea class="" type="text" placeholder="имя"></textarea>
-        </label>
-        <label for="">
-          <textarea type="text" placeholder="номер телефона"></textarea>
-        </label>
-        <label for="">
-          <textarea class="h-40" type="text" placeholder="дополнительная информация"></textarea>
-        </label>
-        <button class="bg-grad p-2.5 rounded-xl text-white hover:drop-shadow-[-2px_13px_23px_rgba(59,56,92,0.2)] hover:scale-[1.01] transition-all">отправить</button>
-      </form>
+    <div ref="orderRef" class=" flex flex-col bg-gray-burger mx-auto w-full max-w-xl h-full rounded-xl p-4">
+        <div class="flex justify-end">
+          <button v-on:click="toggleOrderWindow" class="rounded-xl active:bg-gray-light md:active:bg-gray-burger w-fit p-2.5">
+            <span class="w-fit select-none text-gray-dark">закрыть</span>
+          </button>
+        </div>
+        <form class="flex [&>*>textarea]:resize-none flex-col w-full [&>*>textarea]:text-gray-dark [&>*>textarea]:border-2 [&>*>textarea]:border-white [&>*>textarea:focus]:border-gray-dark [&>*>textarea:focus]:outline-none [&>*>textarea]:focus:border-pink-500 [&>*>textarea]:w-full [&>*>textarea]:rounded-xl [&>*>textarea]:p-2.5 gap-5" action="">
+          <label class="mt-2.5" for="">
+            <textarea class="" type="text" placeholder="имя"></textarea>
+          </label>
+          <label for="">
+            <textarea type="text" placeholder="номер телефона"></textarea>
+          </label>
+          <label for="">
+            <textarea class="" type="text" placeholder="дополнительная информация"></textarea>
+          </label>
+        </form>
+        <button class="bg-grad p-2.5 mt-auto rounded-xl w-full text-white hover:drop-shadow-[-2px_13px_23px_rgba(59,56,92,0.2)] hover:scale-[1.01] transition-all">отправить</button>
     </div>
   </section>
 </template>
