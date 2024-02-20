1dbc6c8bd88edb3739f2254335e2a3c0c0387a9d
Roma Tolkachev <tolkachevroman@bk.ru>
2024-02-20 19:46:17 +0300

    test order

--- src/components/header/OrderHero.vue
+++ src/components/header/OrderHero.vue
@@ -1,16 +1,28 @@
 <script setup>
 import {piniaStorage} from "@/stores/pinia.js";
+import {onClickOutside} from "@vueuse/core";
+import {ref} from "vue";
+import {storeToRefs} from "pinia";
+
 
 const pinia = piniaStorage();
 const toggleOrderWindow = pinia.toggleOrderWindow;
+const { isOrderOpen } = storeToRefs(pinia);
+const orderRef = ref(null);
+
+onClickOutside(orderRef, () => {
+  if (isOrderOpen.value === true) {
+    isOrderOpen.value = false;
+  }
+})
 
 </script>
 
 <template>
   <section class="h-[calc(100svh-65px)] w-full fixed left-1/2 -translate-x-1/2 container py-6">
-    <div class="bg-gray-burger mx-auto w-full max-w-3xl h-full rounded-xl p-2.5">
+    <div ref="orderRef" class="bg-gray-burger mx-auto w-full max-w-3xl h-full rounded-xl p-2.5">
       <div class="flex justify-end">
-        <button v-on:click="toggleOrderWindow" class="rounded-xl active:bg-white w-fit p-2.5">
+        <button v-on:click="toggleOrderWindow" class="rounded-xl active:bg-gray-light md:active:bg-gray-burger w-fit p-2.5">
           <span class="w-fit select-none text-gray-dark">закрыть</span>
         </button>
       </div>
@@ -24,7 +36,7 @@ const toggleOrderWindow = pinia.toggleOrderWindow;
         <label for="">
           <textarea class="h-40" type="text" placeholder="дополнительная информация"></textarea>
         </label>
-        <button class="bg-grad p-2.5 rounded-[10px] text-white">отправить</button>
+        <button class="bg-grad p-2.5 rounded-xl text-white hover:drop-shadow-[-2px_13px_23px_rgba(59,56,92,0.2)] hover:scale-[1.01] transition-all">отправить</button>
       </form>
     </div>
   </section>
