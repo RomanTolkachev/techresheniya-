bd8485a01149f1814df3da48046578f0f760758e
Roma Tolkachev <tolkachevroman@bk.ru>
2024-02-28 21:28:54 +0300

    did slider. need CSS

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -3,6 +3,8 @@ import {piniaStorage} from "@/stores/pinia.js";
 import {ref} from "vue";
 import {products} from '@/assets/products/spare_parts.js'
 import {useRoute, useRouter} from "vue-router";
+import { Splide, SplideSlide } from '@splidejs/vue-splide';
+import '@splidejs/vue-splide/css';
 
 
 const pinia = piniaStorage();
@@ -18,13 +20,21 @@ const item = products.find((item) => item.id === route.params.id)
 
 <template>
   <div class="w-full">
-          <button v-on:click="router.go(-1)" class="block w-fit self-end rounded-xl  p-3.5 z-30 ">
-            <span class="text-20px w-full select-none text-black">назад</span>
-          </button>
-          <div class="">
-              <img class="min-h-full object-cover"
-                   v-bind:src="item.img" alt="">
-          </div>
+    <button v-on:click="router.go(-1)" class="block w-fit self-end rounded-xl  p-3.5 z-30 ">
+      <span class="text-20px w-full select-none text-black">назад</span>
+    </button>
+    <Splide :options="{ rewind: true }" aria-label="My Favorite Images">
+      <SplideSlide>
+        <img src="/public/items/1.jpeg" alt="Sample 1">
+      </SplideSlide>
+      <SplideSlide>
+        <img src="/public/items/2.jpg" alt="Sample 1">
+      </SplideSlide>
+    </Splide>
+<!--          <div class="">-->
+<!--              <img class="min-h-full object-cover"-->
+<!--                   v-bind:src="item.img" alt="">-->
+<!--          </div>-->
   </div>
 </template>
 
