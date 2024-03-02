1e964622c160be144cc7822ddc43aa0bc89e7be5
Roma Tolkachev <tolkachevroman@bk.ru>
2024-03-02 14:31:27 +0300

    did somestyles

---
+++ src/components/svg/ArrowLeft.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <svg class="w-12 h-12 text-gray-dark dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14M5 12l4-4m-4 4 4 4"/>
+  </svg>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -3,6 +3,8 @@ import {piniaStorage} from "@/stores/pinia.js";
 import {computed, ref, watch} from "vue";
 import {products} from '@/assets/products/spare_parts.js'
 import {useRoute, useRouter} from "vue-router";
+import ArrowLeft from "@/components/svg/ArrowLeft.vue";
+import PhoneIcon from "@/components/svg/PhoneIcon.vue";
 
 const pinia = piniaStorage();
 
@@ -37,20 +39,25 @@ watch(() => transitionStyle.value, () => console.log(transitionStyle.value))
 
 <template>
   <div class="w-full h-svh flex flex-col font-Onest bg-gray-light">
-    <button v-on:click="router.go(-1)" class="">
-      <span class="text-20px w-full select-none text-black">назад</span>
-    </button>
+    <div class="flex justify-between w-full max-w-lg mx-auto">
+      <button v-on:click="router.go(-1)" class="p-2">
+        <ArrowLeft class=""></ArrowLeft>
+      </button>
+      <button class="p-2 flex">
+        <span class="leading-3 font-bold text-gray-dark self-center">задать вопрос <br> оператору</span>
+        <PhoneIcon></PhoneIcon>
+      </button>
+    </div>
     <div class="slider w-full max-w-lg aspect-video min-h-80 mx-auto flex overflow-hidden">
-        <div v-for="picture in prodInfo.img"
-             class="dynamic-transition aspect-[1.3] w-full h-full mx-auto flex shrink-0 duration-300 relative">
-          <div
-              v-bind:style="{backgroundImage: `url(${picture})`}"
-              class="custom-background h-full w-full blur-[60px] brightness-125"></div>
-          <img
-              class="h-full mx-auto object-cover absolute right-1/2 translate-x-1/2"
-              :src="picture" alt="">
-        </div>
-
+      <div v-for="picture in prodInfo.img"
+           class="dynamic-transition aspect-[1.3] w-full h-full mx-auto flex shrink-0 duration-300 relative">
+        <div
+            v-bind:style="{backgroundImage: `url(${picture})`}"
+            class="custom-background h-full w-full blur-[60px] brightness-125"></div>
+        <img
+            class="h-full mx-auto object-cover absolute right-1/2 translate-x-1/2"
+            :src="picture" alt="">
+      </div>
     </div>
     <div class="flex justify-center gap-5">
       <button @click="prevSlide">prev</button>
