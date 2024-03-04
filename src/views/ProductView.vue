35f2d3a22875d74bd599445a161219364a3b7477
Roma Tolkachev <tolkachevroman@bk.ru>
2024-03-04 14:21:03 +0300

    mobile slider works. try

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -15,6 +15,14 @@ const prodInfo = products.find((item) => item.id === route.params.id);
 // carousel
 
 const currenSlideIndex = ref(0)
+const startX = ref(0);
+const currentX = ref(0);
+const deltaX = ref(0);
+const sliderRef = ref(null);
+const transitionValue = computed(() => {
+  return -currenSlideIndex.value * slideWidth.value - deltaX.value;
+});
+const slideWidth = ref(0)
 
 const prevSlide = () => {
   currenSlideIndex.value--;
@@ -25,53 +33,52 @@ const nextSlide = () => {
   currenSlideIndex.value > prodInfo.img.length - 1 ? currenSlideIndex.value = 0 : currenSlideIndex.value;
 }
 
-const transitionStyle = computed(() => {
-  return `translateX(${(-currenSlideIndex.value * document.getElementById('slider').offsetWidth - deltaX.value)}px)`
-})
-
 // swiper
-const startX = ref(null);
-const currentX = ref(null);
-const deltaX = ref(0)
 
-
-const handleTouchStart = (event) => {
-  startX.value = event.touches[0].clientX
-  console.log(startX.value);
-}
-const handleTouchMove = (event) => {
-  currentX.value = event.touches[0].clientX
-  deltaX.value = startX.value - currentX.value;
-}
-
-const handleTouchEnd = (event) => {
-  if (deltaX.value < 150 && deltaX.value >= 0 || deltaX.value < 0 && deltaX.value > -150) {
-    deltaX.value = 0;
-  } else if (deltaX.value > 150) {
-    nextSlide();
-    deltaX.value = 0;
-  } else {
-    prevSlide();
-    deltaX.value = 0;
-  }
-}
 
 onMounted(() => {
-  // сломали функцию, когда можно перемещать img при зажатом клике
-  const sliderImage = document.querySelector('img');
-  sliderImage.addEventListener('dragstart', (event) => event.preventDefault())
 
-  const slider = document.getElementById('slider');
+  // сначала получаем ширину слайда, потом следим за изменением ширины слайдера и перезаписываем значение ширины слайдера
+  slideWidth.value = sliderRef.value.offsetWidth;
+  const handleResize = (entries) => {
+    for (const entry of entries) {
+      slideWidth.value = entry.target.offsetWidth
+    }
+  }
+  const observer = new ResizeObserver(handleResize);
+  observer.observe(sliderRef.value, { attributes: true,})
 
-  // мобилка
-  slider.addEventListener('touchstart', handleTouchStart, false);
-  slider.addEventListener('touchmove', handleTouchMove, false);
-  slider.addEventListener('touchend', handleTouchEnd, false);
 
+  const handleTouchstart = (event) => {
+    startX.value = event.touches[0].clientX
+    const slides = document.querySelectorAll('.dynamic-transition');
+    slides.forEach(item => {
+      item.classList.remove('transition-duration')
+    })
+  };
+  const handleTouchmove = (event) => {
+    currentX.value = event.touches[0].clientX;
+    deltaX.value = startX.value - currentX.value
+  };
+  const handleTouchend = () => {
+    const slides = document.querySelectorAll('.dynamic-transition');
+    slides.forEach(item => {
+      item.classList.add('transition-duration')
+    })
+    if (deltaX.value < 70 && deltaX.value >= 0 || deltaX.value < 0 && deltaX.value > -70) {
+      deltaX.value = 0;
+    } else if (deltaX.value > 70) {
+      nextSlide();
+      deltaX.value = 0;
+    } else {
+      prevSlide();
+      deltaX.value = 0;
+    }
+  }
 
-  // ПК
-  // slider.addEventListener('mousedown', handleTouchStart, false)
-  // slider.addEventListener('mousemove', handleTouchMove, false)
+  sliderRef.value.addEventListener('touchstart', handleTouchstart);
+  sliderRef.value.addEventListener('touchmove', handleTouchmove);
+  sliderRef.value.addEventListener('touchend', handleTouchend)
 })
 
 
@@ -80,6 +87,7 @@ onMounted(() => {
 </script>
 
 <template>
+  {{ currenSlideIndex }}
   <div class="w-full h-full flex flex-col font-Onest bg-gray-light">
     <div class="flex justify-between w-full max-w-lg mx-auto">
       <button v-on:click="router.go(-1)" class="p-2">
@@ -90,9 +98,9 @@ onMounted(() => {
         <PhoneIcon></PhoneIcon>
       </button>
     </div>
-    <div id="slider" class="slider w-full max-w-lg aspect-video min-h-80 mx-auto flex overflow-hidden">
+    <div ref="sliderRef" class="slider w-full max-w-lg aspect-video min-h-80 mx-auto flex overflow-hidden">
       <div v-for="picture in prodInfo.img"
-           class="dynamic-transition slide-wrapper aspect-[1.3] w-full h-full mx-auto flex shrink-0 duration-300 relative">
+           class="dynamic-transition transition-duration slide-wrapper w-full aspect-[1.3] h-full mx-auto flex shrink-0 relative">
         <div
             v-bind:style="{backgroundImage: `url(${picture})`}"
             class="custom-background h-full w-full blur-[60px] brightness-125"></div>
@@ -125,11 +133,14 @@ onMounted(() => {
 
 <style scoped>
 .dynamic-transition {
-  transform:v-bind(transitionStyle);
+  transform:v-bind(`translateX(${transitionValue}px)`);
+
 }
+.transition-duration {
+  transition-duration: 300ms;
+}
+
 .custom-background {
   background-size: 100% 100%;
-  //filter: blur(100px);
-
 }
 </style>
